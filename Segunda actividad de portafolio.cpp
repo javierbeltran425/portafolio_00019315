b59d4a94e1d54b3820278687d76cf0cfc7f030e1
javierbeltran425 <00019315@uca.edu.sv>
2019-08-31 10:54:40 -0600

    Actualizacion de segunda actividad -eliminacion de lineas

--- Segunda actividad de portafolio.cpp
+++ Segunda actividad de portafolio.cpp
@@ -5,13 +5,9 @@ float mediana(int a[], int tamano){
 	int mediana;
 	int b = 0;
 	int c = 0;
-	
 	b = tamano/2;
 	c = b+1;
-
 	mediana = (a[b-1] + a[c-1]) / 2; 
-	
-	
 	return mediana;
 	
 }
@@ -21,8 +17,6 @@ int main(){
 	int a[6] = {2,3,6,8,10,14};
 	int tamano = 6;
 	int med = mediana(a, tamano);
-	
 	cout << "La mediana es: " << med;
-	
 	return 0;
 }
